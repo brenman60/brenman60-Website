4476f376c49bc55cf1c2f84e3967871c931fd8a5
brenman60 <83514481+brenman60@users.noreply.github.com>
2023-09-20 13:50:11 -0400

    Button changes

--- js/mouseButtons.js
+++ js/mouseButtons.js
@@ -1,21 +1,33 @@
 ﻿var buttons = [];
 
-function startButtonRotation(element) {
-    if (!buttons.find(element)) {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function startButtonRotation(element) {
+    if (!buttons.includes(element)) {
         buttons.push(element);
     }
 
     do {
         element.style.transform = "rotate(15deg)";
-        element.style.transition = "all .5s";
+        element.style.transition = "transform .5s linear .25";
+
+        await sleep(.25 * 1000); // sleep function for first int seconds
+
+        if (!buttons.includes(element)) {
+            break;
+        }
 
         element.style.transform = "rotate(-15deg)";
-        element.style.transition = "all .5s";
+        element.style.transition = "transform .5s linear .25";
     }
-    while (buttons.find(element));
+    while (buttons.includes(element));
 }
 
 function stopButtonRotation(element) {
     element.style.transform = "rotate(0deg)";
     element.style.transition = "all .5s";
+
+    buttons.pop(element);
 }
