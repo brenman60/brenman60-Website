815bcd3724aefd22156b418ee20935ac33f69491
brenman60 <83514481+brenman60@users.noreply.github.com>
2023-09-20 13:35:36 -0400

    Changes

--- js/mouseButtons.js
+++ js/mouseButtons.js
@@ -1,6 +1,18 @@
-﻿function startButtonRotation(element) {
-    element.style.transform = "rotate(45deg)";
-    element.style.transition = "all .5s";
+﻿var buttons = [];
+
+function startButtonRotation(element) {
+    if (!buttons.find(element)) {
+        buttons.push(element);
+    }
+
+    do {
+        element.style.transform = "rotate(15deg)";
+        element.style.transition = "all .5s";
+
+        element.style.transform = "rotate(-15deg)";
+        element.style.transition = "all .5s";
+    }
+    while (buttons.find(element));
 }
 
 function stopButtonRotation(element) {
