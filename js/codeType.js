9342cfd3a1470c75a0199b53481f2fc0293fc96b
brenman60 <83514481+brenman60@users.noreply.github.com>
2023-09-21 13:12:19 -0400

    Added codeType.js functionality

--- js/codeType.js
+++ js/codeType.js
@@ -2,9 +2,7 @@ var lastTypedIndex = 0;
 
 var allCodes =
     [
-        "",
-        "",
-        ""
+        'if (fullCodeString.length >= lastTypedIndex - 1) { \n document.getElementById("codingArea").innerHTML += fullCodeString[i]; \n } else { \n continue; \n }',
     ];
 
 var fullCodeString = "";
@@ -15,10 +13,10 @@ function getCodeString() {
     if (fullCodeString == "") {
         // first initialize
         lastTypedIndex = -1;
-        finalString = allCodes[Math.random(0, allCodes.length - 1)];
+        finalString = allCodes[Math.floor(Math.random() * allCodes.length)];
     } else if (lastTypedIndex == fullCodeString - 1) {
         lastTypedIndex = -1;
-        finalString = allCodes[Math.random(0, allCodes.length - 1)];
+        finalString = allCodes[Math.floor(Math.random() * allCodes.length)];
     } else {
         finalString = fullCodeString;
     }
@@ -29,11 +27,20 @@ function getCodeString() {
 function typeCode() {
     fullCodeString = getCodeString();
 
+    if (fullCodeString == undefined) {
+        throw new Error("Error retrieving code string.");
+    }
+
+    document.getElementById("codingArea").innerHTML = "";
+    document.getElementById("codingArea").innerHTML = fullCodeString.slice(0, lastTypedIndex);
+
     for (var i = 0; i < 4; i++) {
         lastTypedIndex++;
 
         if (fullCodeString.length >= lastTypedIndex - 1) {
-
+            document.getElementById("codingArea").innerHTML += fullCodeString.charAt(i);
+        } else {
+            continue;
         }
     }
 }
