443c32c0a4dc310fc572ee7ce1acdfcebe9a8d60
brenman60 <83514481+brenman60@users.noreply.github.com>
2023-10-01 17:52:13 -0400

    index.html redesign

--- js/mouseButtons.js
+++ js/mouseButtons.js
@@ -1,11 +1,14 @@
 ﻿function startButtonRotation(element) {
     const randomRot_ = Math.round(Math.random());
-    const randomRot = randomRot_ ? -2.5 : 2.5;
-    element.style.transform = "rotate(" + randomRot + "deg)";
-    element.style.transition = "transform .5s linear";
+    // maybe re add this later idk
+    //element.style.transform = "scale(120%, 120%)";
+    element.style.backgroundColor = "#528789";
+    element.style.transition = "transform .5s ease-out";
 }
 
 function stopButtonRotation(element) {
-    element.style.transform = "rotate(0deg)";
-    element.style.transition = "all .5s .1s";
+    // maybe re add this later idk
+    //element.style.transform = "scale(100%, 100%)";
+    element.style.backgroundColor = "#5F9EA0";
+    element.style.transition = "all .5s ease-in";
 }
