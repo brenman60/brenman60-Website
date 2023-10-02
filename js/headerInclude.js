99d90e770350ee18a5b70cc1c6b9490991b9c079
brenman60 <83514481+brenman60@users.noreply.github.com>
2023-10-01 23:27:12 -0400

    Website hosting & easy header including

---
+++ js/headerInclude.js
@@ -0,0 +1,8 @@
+function includeHeader() {
+    console.log("Including header!");
+	fetch('template/header.html')
+        .then(response => response.text())
+        .then(headerHtml => {
+            document.querySelector('body').insertAdjacentHTML('afterbegin', headerHtml);
+        });
+}
