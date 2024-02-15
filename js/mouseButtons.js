636c29978af8ab494d97c5ada9226a98e6431eba
brenman60 <83514481+brenman60@users.noreply.github.com>
2024-02-15 11:18:43 -0500

    Style testing

--- js/mouseButtons.js
+++ js/mouseButtons.js
@@ -73,20 +73,22 @@ function updateShockwaves() {
 }
 
 function startButtonRotation(element, changeColor = true) {
-    element.style.transform = "scale(105%, 105%)";
+    //element.style.transform = "scale(105%, 105%)";
+    element.style.transform = "position(100%, 95%)";
 
     if (changeColor) {
-        element.style.backgroundColor = "#1b7539";
+        //element.style.backgroundColor = "#1b7539";
     }
 
     element.style.transition = "transform .5s ease-out";
 }
 
 function stopButtonRotation(element, changeColor = true) {
-    element.style.transform = "scale(100%, 100%)";
+    //element.style.transform = "scale(100%, 100%)";
+    element.style.transform = "position(100%, 100%)";
 
     if (changeColor) {
-        element.style.backgroundColor = "#228a45";
+        //element.style.backgroundColor = "#228a45";
     }
 
     element.style.transition = "all .5s ease-in";
